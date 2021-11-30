53fa9ac8b90f4e2c9487cd1e2e99f593efa34226
Bavo Andriessen <bavo.andriessen@ugent.be>
2021-11-30 16:53:37 +0100

    starter project for containermanagement

---
+++ services/containermanagement/src/main/java/com/projectsysdes/containermanagement/ContainermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.projectsysdes.containermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainermanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainermanagementApplication.class, args);
+    }
+
+}
