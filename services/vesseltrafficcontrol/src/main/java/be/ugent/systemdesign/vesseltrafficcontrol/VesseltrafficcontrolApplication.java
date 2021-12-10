5821c5ac7f9a3892fe3bf1c5dbf00c796632dfe9
gevnieuw <geertjan.vannieuwenhove@ugent.be>
2021-12-10 13:13:55 +0100

    Vessel Traffic Control

---
+++ services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/VesseltrafficcontrolApplication.java
@@ -0,0 +1,13 @@
+package be.ugent.systemdesign.vesseltrafficcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesseltrafficcontrolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VesseltrafficcontrolApplication.class, args);
+    }
+
+}
---
+++ services/vesseltrafficcontrol/src/test/java/be/ugent/systemdesign/vesseltrafficcontrol/VesseltrafficcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ugent.systemdesign.vesseltrafficcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesseltrafficcontrolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
