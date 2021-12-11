5c93eacc2d6d6917307d63bb457d4306c6817201
gevnieuw <geertjan.vannieuwenhove@ugent.be>
2021-12-11 15:03:46 +0100

    testing repository

--- services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/VesseltrafficcontrolApplication.java
+++ services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/VesseltrafficcontrolApplication.java
@@ -1,13 +1,20 @@
 package be.ugent.systemdesign.vesseltrafficcontrol;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class VesseltrafficcontrolApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(VesseltrafficcontrolApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(VesseltrafficcontrolApplication.class, args);
     }
+
+
 
 }
