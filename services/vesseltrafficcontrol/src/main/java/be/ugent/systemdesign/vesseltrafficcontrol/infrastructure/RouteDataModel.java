c484df6867e9076e7ac5f3ae8d5180514afad36f
gevnieuw <geertjan.vannieuwenhove@ugent.be>
2021-12-11 15:01:25 +0100

    Added datamodel repository

---
+++ services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/infrastructure/RouteDataModel.java
@@ -0,0 +1,40 @@
+package be.ugent.systemdesign.vesseltrafficcontrol.infrastructure;
+
+import be.ugent.systemdesign.vesseltrafficcontrol.domain.enums.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class RouteDataModel {
+
+    @Id
+    @Getter
+    private Integer routeId;
+    @Getter
+    private Size sizeCompatibility;
+
+    private Integer cost;
+    private String route;
+    private Integer routeLength;
+    private Integer capacity;
+
+    public RouteDataModel(Integer id, String route, Size sizeCompatibility, Integer routeLength, Integer capacity) {
+        this.routeId = id;
+        this.route = route;
+        this.sizeCompatibility = sizeCompatibility;
+        this.routeLength = routeLength;
+        this.capacity = capacity;
+        this.calculateCost();
+    }
+
+    private void calculateCost(){
+        this.cost = 0;
+    }
+}
