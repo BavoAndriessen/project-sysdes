32040d3c687d519e2c378104cefc6f323c808386
gevnieuw <geertjan.vannieuwenhove@ugent.be>
2021-12-12 18:11:24 +0100

    added restController

---
+++ services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/api/VTCRestController.java
@@ -0,0 +1,39 @@
+package be.ugent.systemdesign.vesseltrafficcontrol.api;
+
+import be.ugent.systemdesign.vesseltrafficcontrol.application.Response;
+import be.ugent.systemdesign.vesseltrafficcontrol.application.ResponseStatus;
+import be.ugent.systemdesign.vesseltrafficcontrol.application.VTCService;
+import be.ugent.systemdesign.vesseltrafficcontrol.domain.aggregates.Vessel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins="*")
+@RequestMapping(path = "api/vtc/")
+public class VTCRestController {
+
+    @Autowired
+    VTCService vtcService;
+
+    @PostMapping
+    public ResponseEntity<String> registerShip(Vessel vessel) {
+        Response response = vtcService.registerVessel(vessel);
+        if(response.status == ResponseStatus.FAIL) {
+            return new ResponseEntity<String>(response.message, HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<String>(response.message, HttpStatus.OK);
+    }
+
+    @PutMapping("gate/{id}")
+    public ResponseEntity<String> gateStateChanged(@PathVariable("id") Integer gateId) {
+        Response response = vtcService.changeGateState(gateId);
+        if(response.status == ResponseStatus.FAIL) {
+            return new ResponseEntity<String>(response.message, HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<String>(response.message, HttpStatus.OK);
+    }
+
+
+}
