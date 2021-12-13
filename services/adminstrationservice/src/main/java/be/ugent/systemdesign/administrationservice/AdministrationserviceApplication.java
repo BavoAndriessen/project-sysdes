547221c6f03f90c603e1585f100f506dd60ea0de
Ellen Mullie <ellen.mullie@ugent.be>
2021-12-13 12:56:50 +0100

    dockerfile

--- services/adminstrationservice/src/main/java/be/ugent/systemdesign/administrationservice/AdministrationserviceApplication.java
+++ services/adminstrationservice/src/main/java/be/ugent/systemdesign/administrationservice/AdministrationserviceApplication.java
@@ -30,108 +30,108 @@ public class AdministrationserviceApplication {
 
     private static final Logger log = LoggerFactory.getLogger(AdministrationserviceApplication.class);
 
-    @Bean
-    public CommandLineRunner populateDatabase(DocumentRepository repo, StaffRepository staffRepo) {
-        return (args) -> {
-            //DMrepo.deleteAll();
-            Container c1 = new Container(1, "bananen");
-            Container c2 = new Container(2, "appels");
-            Container c3 = new Container(3, "wortels");
-
-            List<Container> containerList = new ArrayList<>();
-            containerList.add(c1);
-            containerList.add(c2);
-            containerList.add(c3);
-
-            Offer offer = new Offer( 200.7, 2, null, 5.0, 6.0);
-
-            Vessel vessel = new Vessel("ellen", null);
-
-            Document document = new Document( vessel, offer, containerList);
-
-            document = repo.save(document);
-
-            List<Document> documents = repo.findAllDocumentsForVessel(vessel.getVesselId());
-            Document docs = documents.get(0);
-
-            log.warn("Document found with id {} and offer {}", docs.getDocumentId(), docs.getOffer().getPrice());
-
-            WorkedHours wh1 = new WorkedHours(LocalDateTime.now(), LocalDateTime.now());
-            WorkedHours wh2 = new WorkedHours(LocalDateTime.now(), LocalDateTime.now());
-            WorkedHours wh3 = new WorkedHours(LocalDateTime.now(), LocalDateTime.now());
-            List<WorkedHours> workedHours = new ArrayList<>();
-            workedHours.add(wh1);
-            workedHours.add(wh2);
-            workedHours.add(wh3);
-
-            Staff s = new Staff(1, "mullie", "ellen", StaffType.PILOT, 10.2, workedHours);
-
-            staffRepo.save(s);
-
-            Staff staff = staffRepo.findOne(1);
-
-            log.warn("Staff found with id {} and wage {}", staff.getStaffId(), staff.getWage());
-        };
-    }
-
-    @Bean
-    public CommandLineRunner testStaff(){
-        return (args -> {
-            Staff s = new Staff(1, "mullie", "ellen", StaffType.PILOT, 10.2, null);
-            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 23, 8,0));
-            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 23, 17,0));
-            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 24, 8,0));
-            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 24, 17,0));
-            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 25, 8,0));
-
-            try{
-                log.warn("Ellen needs to be paid {} euros", s.getPay());
-            }
-            catch(shiftNotOverExecption e){
-                log.warn("Ellen can not be payed if their shift is not over yet");
-            }
-            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 25, 17,0));
-            try{
-                log.warn("Ellen needs to be paid {} euros", s.getPay());
-            }
-            catch(shiftNotOverExecption e){
-                log.warn("Ellen can not be payed if their shift is not over yet");
-            }
-        });
-    }
-
-    @Bean
-    public CommandLineRunner testApplicationService(AdministrationServiceImpl service){
-        return (args -> {
-            service.addStaff(new Staff(5, "Mullie", "Ellen", StaffType.DOCKWORKER, 13.5, null));
-            Response r1 = service.registerNewTimeBadge(5, LocalDateTime.of(2021, Month.JUNE, 25, 17,0));
-            log.warn(r1.getMessage());
-
-            Container c1 = new Container(1, "bananen");
-            Container c2 = new Container(2, "appels");
-            Container c3 = new Container(3, "wortels");
-
-            List<Container> containerList = new ArrayList<>();
-            containerList.add(c1);
-            containerList.add(c2);
-
-
-            OfferCreatedResponse r2 = service.registerNewVesselWithOffer("vessel3", LocalDateTime.now(), 50, 80.0, 60.5, containerList);
-            log.warn(r2.getMessage());
-            containerList.add(c3);
-            OfferCreatedResponse r3 = service.registerNewVesselWithOffer("vessel3", LocalDateTime.now(), 25, 80.0, 60.5, containerList);
-            log.warn(r3.getMessage());
-
-            Response r4 = service.handleOfferDeleted("vessel3", r3.getOfferId());
-            log.warn(r4.getMessage());
-
-            Response verkeerdeVessel = service.handleOfferDeleted("vessel4", r3.getOfferId());
-            Response verkeerdeOffer = service.handleOfferDeleted("vessel3", 2);
-            Response verkeerdeStaff = service.registerNewTimeBadge(3, LocalDateTime.now());
-            log.warn(verkeerdeOffer.getMessage());
-            log.warn(verkeerdeVessel.getMessage());
-            log.warn(verkeerdeStaff.getMessage());
-        });
-    }
+//    @Bean
+//    public CommandLineRunner populateDatabase(DocumentRepository repo, StaffRepository staffRepo) {
+//        return (args) -> {
+//            //DMrepo.deleteAll();
+//            Container c1 = new Container(1, "bananen");
+//            Container c2 = new Container(2, "appels");
+//            Container c3 = new Container(3, "wortels");
+//
+//            List<Container> containerList = new ArrayList<>();
+//            containerList.add(c1);
+//            containerList.add(c2);
+//            containerList.add(c3);
+//
+//            Offer offer = new Offer( 200.7, 2, null, 5.0, 6.0);
+//
+//            Vessel vessel = new Vessel("ellen", null);
+//
+//            Document document = new Document( vessel, offer, containerList);
+//
+//            document = repo.save(document);
+//
+//            List<Document> documents = repo.findAllDocumentsForVessel(vessel.getVesselId());
+//            Document docs = documents.get(0);
+//
+//            log.warn("Document found with id {} and offer {}", docs.getDocumentId(), docs.getOffer().getPrice());
+//
+//            WorkedHours wh1 = new WorkedHours(LocalDateTime.now(), LocalDateTime.now());
+//            WorkedHours wh2 = new WorkedHours(LocalDateTime.now(), LocalDateTime.now());
+//            WorkedHours wh3 = new WorkedHours(LocalDateTime.now(), LocalDateTime.now());
+//            List<WorkedHours> workedHours = new ArrayList<>();
+//            workedHours.add(wh1);
+//            workedHours.add(wh2);
+//            workedHours.add(wh3);
+//
+//            Staff s = new Staff(1, "mullie", "ellen", StaffType.PILOT, 10.2, workedHours);
+//
+//            staffRepo.save(s);
+//
+//            Staff staff = staffRepo.findOne(1);
+//
+//            log.warn("Staff found with id {} and wage {}", staff.getStaffId(), staff.getWage());
+//        };
+//    }
+//
+//    @Bean
+//    public CommandLineRunner testStaff(){
+//        return (args -> {
+//            Staff s = new Staff(1, "mullie", "ellen", StaffType.PILOT, 10.2, null);
+//            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 23, 8,0));
+//            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 23, 17,0));
+//            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 24, 8,0));
+//            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 24, 17,0));
+//            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 25, 8,0));
+//
+//            try{
+//                log.warn("Ellen needs to be paid {} euros", s.getPay());
+//            }
+//            catch(shiftNotOverExecption e){
+//                log.warn("Ellen can not be payed if their shift is not over yet");
+//            }
+//            s.addWorkedHour(LocalDateTime.of(2021, Month.JUNE, 25, 17,0));
+//            try{
+//                log.warn("Ellen needs to be paid {} euros", s.getPay());
+//            }
+//            catch(shiftNotOverExecption e){
+//                log.warn("Ellen can not be payed if their shift is not over yet");
+//            }
+//        });
+//    }
+//
+//    @Bean
+//    public CommandLineRunner testApplicationService(AdministrationServiceImpl service){
+//        return (args -> {
+//            service.addStaff(new Staff(5, "Mullie", "Ellen", StaffType.DOCKWORKER, 13.5, null));
+//            Response r1 = service.registerNewTimeBadge(5, LocalDateTime.of(2021, Month.JUNE, 25, 17,0));
+//            log.warn(r1.getMessage());
+//
+//            Container c1 = new Container(1, "bananen");
+//            Container c2 = new Container(2, "appels");
+//            Container c3 = new Container(3, "wortels");
+//
+//            List<Container> containerList = new ArrayList<>();
+//            containerList.add(c1);
+//            containerList.add(c2);
+//
+//
+//            OfferCreatedResponse r2 = service.registerNewVesselWithOffer("vessel3", LocalDateTime.now(), 50, 80.0, 60.5, containerList);
+//            log.warn(r2.getMessage());
+//            containerList.add(c3);
+//            OfferCreatedResponse r3 = service.registerNewVesselWithOffer("vessel3", LocalDateTime.now(), 25, 80.0, 60.5, containerList);
+//            log.warn(r3.getMessage());
+//
+//            Response r4 = service.handleOfferDeleted("vessel3", r3.getOfferId());
+//            log.warn(r4.getMessage());
+//
+//            Response verkeerdeVessel = service.handleOfferDeleted("vessel4", r3.getOfferId());
+//            Response verkeerdeOffer = service.handleOfferDeleted("vessel3", 2);
+//            Response verkeerdeStaff = service.registerNewTimeBadge(3, LocalDateTime.now());
+//            log.warn(verkeerdeOffer.getMessage());
+//            log.warn(verkeerdeVessel.getMessage());
+//            log.warn(verkeerdeStaff.getMessage());
+//        });
+//    }
 
 }
