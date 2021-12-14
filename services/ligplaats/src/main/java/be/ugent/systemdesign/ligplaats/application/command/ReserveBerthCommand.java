8bb5b76d6e0ba8f5e6dfab359d9fd5511d3843d9
Ahmad Alkaddor <ahmad.alkaddor@ugent.be>
2021-12-14 18:00:03 +0100

    docker 5

--- services/ligplaats/src/main/java/be/ugent/systemdesign/ligplaats/application/command/ReserveBerthCommand.java
+++ services/ligplaats/src/main/java/be/ugent/systemdesign/ligplaats/application/command/ReserveBerthCommand.java
@@ -1,5 +1,6 @@
 package be.ugent.systemdesign.ligplaats.application.command;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReserveBerthCommand {
     private String vesselId;
     private Double vesselSize;
--- services/ligplaats/src/main/java/be/ugent/systemdesign/ligplaats/application/command/ReserveBerthResponse.java
+++ services/ligplaats/src/main/java/be/ugent/systemdesign/ligplaats/application/command/ReserveBerthResponse.java
@@ -2,6 +2,7 @@ package be.ugent.systemdesign.ligplaats.application.command;
 
 import be.ugent.systemdesign.ligplaats.application.Response;
 import be.ugent.systemdesign.ligplaats.application.ResponseStatus;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReserveBerthResponse extends Response {
 
     String vesselId;
