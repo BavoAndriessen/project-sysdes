5827e373e05b3ad70de8afc3be85aeda40daf4da
gevnieuw <geertjan.vannieuwenhove@ugent.be>
2021-12-14 17:08:39 +0100

    enable async

--- services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/VesseltrafficcontrolApplication.java
+++ services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/VesseltrafficcontrolApplication.java
@@ -6,9 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
-
+@EnableAsync
 @EnableBinding(Channels.class)
 @SpringBootApplication
 public class VesseltrafficcontrolApplication {
