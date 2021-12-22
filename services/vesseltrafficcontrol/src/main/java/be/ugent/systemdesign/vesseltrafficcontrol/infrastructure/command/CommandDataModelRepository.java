8b30bea111bebfd9f9d48ea294433f16b0d5726d
gevnieuw <geertjan.vannieuwenhove@ugent.be>
2021-12-22 13:08:33 +0100

    fixed could not autowire error

--- services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/infrastructure/command/CommandDataModelRepository.java
+++ services/vesseltrafficcontrol/src/main/java/be/ugent/systemdesign/vesseltrafficcontrol/infrastructure/command/CommandDataModelRepository.java
@@ -1,6 +1,8 @@
 package be.ugent.systemdesign.vesseltrafficcontrol.infrastructure.command;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface CommandDataModelRepository<T extends DomainCommandDataModel> extends JpaRepository<T, Integer> {
 }
